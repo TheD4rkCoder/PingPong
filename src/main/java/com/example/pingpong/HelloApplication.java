69540cae86bf8ec724df748a46714dd839c9108e
TheD4rkCoder <david2mairhofer@gmail.com>
2023-01-11 08:42:50 +0100

    initial

--- src/main/java/com/example/pingpong/HelloApplication.java
+++ src/main/java/com/example/pingpong/HelloApplication.java
@@ -2,7 +2,11 @@ package com.example.pingpong;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +14,32 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Group group = new Group();
+        Rectangle r = new Rectangle(800, 500);
+        r.setFill(Paint.valueOf("green"));
+        r.setStroke(Paint.valueOf("blue"));
+        r.setStrokeWidth(20);
+        r.setX(10);
+        r.setY(10);
+        group.getChildren().add(r);
+        Rectangle r2 = new Rectangle(20, 100);
+        r2.setFill(Paint.valueOf("yellow"));
+        r2.setStroke(Paint.valueOf("black"));
+        r2.setStrokeWidth(5);
+        r2.setX(30);
+        r2.setY(100);
+        group.getChildren().add(r2);
+        Rectangle r3 = new Rectangle(20, 100);
+        r3.setFill(Paint.valueOf("red"));
+        r3.setStroke(Paint.valueOf("black"));
+        r3.setStrokeWidth(5);
+        r3.setX(770);
+        r3.setY(200);
+        group.getChildren().add(r3);
+
+
+        Scene scene = new Scene(group);
+        stage.setTitle("Ping Pong!");
         stage.setScene(scene);
         stage.show();
     }
